d127f4d9a3543939ba2ff39bbd87a282b94a15f8
furenku <furenku@gmail.com>
2019-10-14 19:34:13 -0500

    colocar codigo HTML basico

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,75 @@
-<script>
-	export let name;
-</script>
+<header id="cabecera_principal">
+	<img src="http://fakeimg.pl/300x100" alt="Logotipo" class="logo" />
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+	<nav class="menu">
+	<ul>
+		<li>
+		<a href="#">
+			Menú 1
+		</a>
+		</li>
+		<li>
+		<a href="#">
+			Menú 2
+		</a>
+		</li>
+		<li>
+		<a href="#">
+			Menú 3
+		</a>
+		</li>
+	</ul>
+	</nav>
+</header>
 
-<h1>Hello {name}!</h1>
+<section class="portada">
+	<h1> PORTADA </h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius voluptatum nisi obcaecati suscipit id atque
+		excepturi non quod vel laboriosam qui fugit ea, voluptate ut dolorem ullam dignissimos quibusdam sint?</p>
+	<img src="https://media.giphy.com/media/13gvXfEVlxQjDO/giphy.gif"
+		alt="Imagen introduccion"
+	>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="https://www.facebook.com/..." TARGET="_blank">
+					<i class="fa fa-facebook"></i>
+				</a>
+			</li>
+			<li>
+				<a href="https://www.twitter.com/..." TARGET="_blank">
+					<i class="fa fa-twitter"></i>
+				</a>
+			</li>
+			<li>
+				<a href="https://www.instagram.com/..." TARGET="_blank">
+					<i class="fa fa-instagram"></i>
+				</a>
+			</li>
+		</ul>
+	</nav>
+</section>
+
+<section class="introduccion">
+	
+	<iframe
+	width="560"
+	height="315"
+	src="https://www.youtube.com/embed/C0DPdy98e4c"
+	frameborder="0"
+	allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+	></iframe>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus error
+		quaerat laboriosam cum consequuntur, debitis eum aperiam iusto, enim
+		reiciendis quas autem eaque odio iste at minus perspiciatis sequi.
+		Soluta.
+	</p>
+</section>
+
+<section class="contenido">
+	Aquí va a ir el Contenido de ejemplo
+</section>
